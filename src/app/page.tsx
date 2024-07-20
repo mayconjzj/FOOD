78e216b99498eabfbb6b084df5d48e7460f7939b
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-07-19 21:55:55 -0400

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,6 +60,7 @@ export default function Home() {
             src="/images/hero.png"
             width={600}
             height={600}
+            unoptimized
             className="w-fit h-full"
           />
         </div>
